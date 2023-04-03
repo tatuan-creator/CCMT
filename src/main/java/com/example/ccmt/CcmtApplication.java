2f2036d3dcb97d94eac9856499bff44e2e29c489
tuan nguyen <taikhoanrac14789@gmail.com>
2023-04-03 08:49:02 +0700

    init project

---
+++ src/main/java/com/example/ccmt/CcmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.ccmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcmtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ccmt/CcmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ccmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
